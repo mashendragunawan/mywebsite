b1c595598fb58853c7f1f64a9b47c0d9f6d791e8
mashendra <mashendragunawan04@gmail.com>
2024-08-13 12:56:56 +0700

    Penambahan Gambar dinamis

--- index.php
+++ index.php
@@ -13,8 +13,8 @@
     ?>
     <div class="container">
         <div class="utama">
-        <img src="background.jpg" alt="">
-        <p><?php echo $d['tag']; ?></p>
+        <?php echo"<img src='".$d['gambar']."' alt=''>";?>
+        <p class="tag"><?php echo $d['tag']; ?></p>
         <h3><?php echo $d['judul']; ?></h3>
         <p><?php echo $d['tangal']; ?></p>
         <button>Read More</button>
@@ -30,17 +30,34 @@
         height:300px; 
     }
     .container{
-        width:250px;
-        height:300px;
+        width:290px;
+        height:420px;
         margin:0px 8px;
-        background-color:red;
+        background-color:#fff;
+        box-shadow: 5px 5px 0px rgba(0, 0, 0, 0.2);
+        border-radius:5px 5px 0px 0px;
         display:inline-flex;
     }
     img{
-        width:250px;
-        height:150px;
+        width:290px;
+        height:250px;
+        object-fit: cover;
+        border-radius:5px 5px 0px 0px;
     }
     h3, p{
+        margin:10px 10px;
+    }
+    .tag{
+        background-color: rgba(9, 132, 227, 0.5);
+        width: 60px;
+        height:28px;
+        display:flex;
+        justify-content:center;
+        align-items:center;
+        font-size:15px; 
+        font-weight: bold;  
+    }
+    button{
         margin:10px 10px;
     }
 </style>
